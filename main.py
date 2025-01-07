fe45989c2683853821d1a4e7f441858f767d9a77
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-01-07 17:44:53 +0000

    dashed line challenge

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from turtle import Turtle, Screen
+import heroes
+
 
 tim = Turtle()
 tim.shape("turtle")
 tim.color("blue")
 
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
+
+for _ in range(10):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
 
 
