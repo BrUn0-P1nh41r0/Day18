dc12bb956f8a09a82ebbe929c157cd9fa30e00ae
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-01-08 17:07:35 +0000

    color palet for hirst

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-from turtle import Turtle, Screen, colormode
+#import colorgram
 import random
+from turtle import Turtle, Screen, colormode
 
 tim = Turtle()
-tim.shape("turtle")
 colormode(255)
 tim.speed(10)
 
-def random_color():
-    r = random.randint(0,255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    color = (r, g, b)
-    return color
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 37)
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
-def draw_spirograph(size_of_gap):
-    for _ in range(int(360 / size_of_gap)):
-        tim.color(random_color())
-        tim.circle(100)
-        tim.setheading(tim.heading() + size_of_gap)
+color_list = [(202, 164, 110), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102), (66, 64, 60), (219, 178, 183), (178, 198, 202), (112, 139, 141), (254, 194, 0)]
 
-draw_spirograph(5)
 
 screen = Screen()
 screen.exitonclick()
+
