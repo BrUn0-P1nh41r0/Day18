ea5e768b28d7c67c4cb5af3778f50108560a8015
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-01-08 11:35:47 +0000

    spirograph

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ import random
 tim = Turtle()
 tim.shape("turtle")
 colormode(255)
-tim.pensize(7)
-turn = [0, 90, 180, 270]
 tim.speed(10)
 
 def random_color():
     r = random.randint(0,255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    colours = (r, g, b)
-    return colours
+    color = (r, g, b)
+    return color
 
-for steps in range(150):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(turn))
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spirograph(5)
 
 screen = Screen()
 screen.exitonclick()
